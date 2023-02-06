236b78f218dc782f3a5675dee32da06ce05f3989
liangmou <1414733123@qq.com>
2023-02-06 17:17:53 +0800

    feat: select组件

---
+++ src/components/Select/index.ts
@@ -0,0 +1,6 @@
+import {withInstall} from "../util/type";
+import Select from "./select.vue";
+
+export const JLSelect = withInstall(Select)
+
+export default JLSelect
---
+++ src/components/Select/select.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="custom-select">
+    <Select v-bind="baseProps" :showSearch="props.filterable" :filter-option="filterOption" :style="{width: props.width}">
+      <slot name="default"/>
+    </Select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Select } from 'ant-design-vue'
+import { defineProps } from 'vue'
+import { SelectProps } from './selectTypes'
+import { omit } from 'lodash'
+
+const props = defineProps(SelectProps)
+const baseProps = omit(props, ['width', 'filterable'])
+
+const filterOption = (input: string, option: any) => {
+  const str = option.label ? 'label' : 'value'
+  return String(option[str]).toLowerCase().indexOf(input.toLowerCase()) >= 0
+}
+
+</script>
+
+<style scoped lang="less">
+@import './style/index.less';
+</style>
