5374f5e0c0e6b976329629906df5a569d02dc20f
100006368 <1234.com>
2023-02-07 15:07:36 +0800

    feat: 完成Divider组件

---
+++ src/components/Divider/divider.vue
@@ -0,0 +1,21 @@
+<template>
+    <Divider v-bind="props" class="jl-divider" />
+</template>
+
+<script lang="ts" setup>
+import { Divider } from 'ant-design-vue';
+import { dividerProps } from 'ant-design-vue/es/divider';
+import { defineProps } from 'vue';
+
+// 定义属性
+const props = defineProps({
+    ...dividerProps(),
+});
+</script>
+
+<style scoped lang="less">
+.jl-divider {
+    height: 1px;
+    background-color: #e9e9e9;
+}
+</style>
---
+++ src/components/Divider/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '../util/type';
+import Divider from './divider.vue';
+
+export const JLDivider = withInstall(Divider);
+
+export default JLDivider;
