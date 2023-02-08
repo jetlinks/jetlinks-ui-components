dcf7982045b4b99cb051365f182675d44c46248d
xieyonghong <18010623010@163.com>
2023-02-08 17:38:09 +0800

    update: 优化组件注册方法

--- components/util/type.ts
+++ components/util/type.ts
@@ -1,10 +1,10 @@
 import type { App, Plugin } from 'vue';
 
-export const withInstall = <T>(comp: T) => {
+export const withInstall = <T>(comp: T, name: string) => {
     const c = comp as any;
     c.install = function (app: App) {
         // @ts-ignore
-        app.component(comp.name, comp);
+        app.component(name || comp.name, comp);
     };
 
     return comp as T & Plugin;
