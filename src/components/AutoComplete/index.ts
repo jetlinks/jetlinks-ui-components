d3e690849a5d862206b34366baaebfaf4955f11a
liangmou <1414733123@qq.com>
2023-02-08 09:41:13 +0800

    feat: 新增AutoComplete组件

---
+++ src/components/AutoComplete/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from "../util/type";
+import AutoComplete from "./autoComplete.vue";
+
+export const JLAutoComplete= withInstall(AutoComplete)
+
+export default JLAutoComplete
