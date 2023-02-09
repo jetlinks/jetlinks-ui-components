b4303b7cd0febbaedb9ff4d4a8dba1b015df5b5b
jackhoo_98 <jackhoo_98@foxmail.com>
2023-02-09 16:21:09 +0800

    update: 更新AIcon组件，支持自定义图标

--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -3,7 +3,7 @@ export default [
   
   {
     path: 'AIcon',
-    meta: {"category":"Components","subtitle":"图标","cols":1,"type":"导航","title":"Icon"},
+    meta: {"category":"Components","subtitle":"图标","cols":1,"type":"通用","title":"Icon","cover":"https://gw.alipayobjects.com/zos/alicdn/rrwbSt3FQ/Icon.svg"},
     component: () => import('../../../components/AIcon/demo/index.vue'),
   },
   {
