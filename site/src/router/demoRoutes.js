1f9c306981adbc84490098b4cd5390770690de90
haodd0216 <1903289197@qq.com>
2023-02-09 15:55:23 +0800

    update: empty组件更新props参数，添加示例demo

--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -3,8 +3,13 @@ export default [
   
   {
     path: 'AIcon',
-    meta: {"category":"Components","subtitle":"图标","cols":1,"type":"导航","title":"Icon"},
+    meta: {"category":"Components","subtitle":"图标","cols":1,"type":"通用","title":"Icon","cover":"https://gw.alipayobjects.com/zos/alicdn/rrwbSt3FQ/Icon.svg"},
     component: () => import('../../../components/AIcon/demo/index.vue'),
+  },
+  {
+    path: 'Empty',
+    meta: {"category":"Components","type":"数据展示","title":"Empty","subtitle":"空状态","cover":"https://gw.alipayobjects.com/zos/alicdn/MNbKfLBVb/Empty.svg"},
+    component: () => import('../../../components/Empty/demo/index.vue'),
   },
   {
     path: 'Pagination',
