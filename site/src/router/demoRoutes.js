9d87f6f497bfa5c6b8f99e50f9aa90489fefd392
haodd0216 <1903289197@qq.com>
2023-02-09 14:32:28 +0800

    update: tablecard组件更新参数设置

--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -5,5 +5,10 @@ export default [
     path: 'Pagination',
     meta: {"category":"Components","subtitle":"分页","cols":1,"type":"导航","title":"Pagination","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
     component: () => import('../../../components/Pagination/demo/index.vue'),
+  },
+  {
+    path: 'TableCard',
+    meta: {"category":"Components","subtitle":"表格卡片","cols":1,"type":"导航","title":"TableCard","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
+    component: () => import('../../../components/TableCard/demo/index.vue'),
   }
 ];
