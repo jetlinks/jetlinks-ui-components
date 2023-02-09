2fc6f3493b4adc9de3c72b62a26c2bda2e5790cd
liangmou <1414733123@qq.com>
2023-02-09 15:01:42 +0800

    feat: 新增Tree组件

--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -10,5 +10,10 @@ export default [
     path: 'Pagination',
     meta: {"category":"Components","subtitle":"分页","cols":1,"type":"导航","title":"Pagination","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
     component: () => import('../../../components/Pagination/demo/index.vue'),
+  },
+  {
+    path: 'Tree',
+    meta: {"category":"Components","type":"数据展示","title":"Tree","subtitle":"树形控件","cover":"https://gw.alipayobjects.com/zos/alicdn/Xh-oWqg9k/Tree.svg"},
+    component: () => import('../../../components/Tree/demo/index.vue'),
   }
 ];
