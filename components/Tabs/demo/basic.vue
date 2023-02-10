87e27d7ac72cdd126b2ac4e48615fa575e373b9c
haodd0216 <1903289197@qq.com>
2023-02-10 14:43:15 +0800

    feat: tabs组件

---
+++ components/Tabs/demo/basic.vue
@@ -0,0 +1,30 @@
+<docs>
+---
+order: 0
+title: 基本用法
+---
+
+默认选中第一项。
+
+
+</docs>
+
+<template>
+  <j-tabs v-model:activeKey="activeKey">
+    <j-tab-pane key="1" tab="Tab 1">Content of Tab Pane 1</j-tab-pane>
+    <j-tab-pane key="2" tab="Tab 2" force-render>Content of Tab Pane 2</j-tab-pane>
+    <j-tab-pane key="3" tab="Tab 3">Content of Tab Pane 3</j-tab-pane>
+  </j-tabs>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+      activeKey: ref('1'),
+    };
+  },
+});
+</script>
+  
--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -7,6 +7,16 @@ export default [
     component: () => import('../../../components/AIcon/demo/index.vue'),
   },
   {
+    path: 'AutoComplete',
+    meta: {"category":"Components","subtitle":"自动完成","type":"数据录入","cols":2,"title":"AutoComplete","cover":"https://gw.alipayobjects.com/zos/alicdn/qtJm4yt45/AutoComplete.svg"},
+    component: () => import('../../../components/AutoComplete/demo/index.vue'),
+  },
+  {
+    path: 'Empty',
+    meta: {"category":"Components","type":"数据展示","title":"Empty","subtitle":"空状态","cover":"https://gw.alipayobjects.com/zos/alicdn/MNbKfLBVb/Empty.svg"},
+    component: () => import('../../../components/Empty/demo/index.vue'),
+  },
+  {
     path: 'Input',
     meta: {"category":"Components","type":"数据录入","title":"Input","subtitle":"输入框","cols":1,"cover":"https://gw.alipayobjects.com/zos/alicdn/xS9YEJhfe/Input.svg"},
     component: () => import('../../../components/Input/demo/index.vue'),
@@ -17,14 +27,24 @@ export default [
     component: () => import('../../../components/Message/demo/index.vue'),
   },
   {
+    path: 'Pagination',
+    meta: {"category":"Components","subtitle":"分页","cols":1,"type":"导航","title":"Pagination","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
+    component: () => import('../../../components/Pagination/demo/index.vue'),
+  },
+  {
     path: 'MonacoEditor',
     meta: {"category":"Components","type":"数据录入","title":"MonacoEditor","subtitle":"代码编辑器","cols":1,"cover":"https://gw.alipayobjects.com/zos/alicdn/xS9YEJhfe/Input.svg"},
     component: () => import('../../../components/MonacoEditor/demo/index.vue'),
   },
   {
-    path: 'Pagination',
-    meta: {"category":"Components","subtitle":"分页","cols":1,"type":"导航","title":"Pagination","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
-    component: () => import('../../../components/Pagination/demo/index.vue'),
+    path: 'TableCard',
+    meta: {"category":"Components","subtitle":"表格卡片","cols":2,"type":"导航","title":"TableCard","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
+    component: () => import('../../../components/TableCard/demo/index.vue'),
+  },
+  {
+    path: 'Tabs',
+    meta: {"category":"Components","subtitle":"标签页","type":"数据展示","title":"Tabs","cover":"https://gw.alipayobjects.com/zos/antfincdn/lkI2hNEDr2V/Tabs.svg"},
+    component: () => import('../../../components/Tabs/demo/index.vue'),
   },
   {
     path: 'Tree',
@@ -32,23 +52,8 @@ export default [
     component: () => import('../../../components/Tree/demo/index.vue'),
   },
   {
-    path: 'AutoComplete',
-    meta: {"category":"Components","subtitle":"自动完成","type":"数据录入","cols":2,"title":"AutoComplete","cover":"https://gw.alipayobjects.com/zos/alicdn/qtJm4yt45/AutoComplete.svg"},
-    component: () => import('../../../components/AutoComplete/demo/index.vue'),
-  },
-  {
     path: 'Scrollbar',
     meta: {"category":"Components","subtitle":"滚动条","cols":1,"type":"导航","title":"Scrollbar","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Scrollbar.svg"},
     component: () => import('../../../components/Scrollbar/demo/index.vue'),
-  },
-  {
-    path: 'TableCard',
-    meta: {"category":"Components","subtitle":"表格卡片","cols":1,"type":"数据展示","title":"TableCard","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
-    component: () => import('../../../components/TableCard/demo/index.vue'),
-  },
-  {
-    path: 'Empty',
-    meta: {"category":"Components","type":"数据展示","title":"Empty","subtitle":"空状态","cover":"https://gw.alipayobjects.com/zos/alicdn/MNbKfLBVb/Empty.svg"},
-    component: () => import('../../../components/Empty/demo/index.vue'),
   }
 ];
