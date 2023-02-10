abbd0284be5ee8262c869861436ff67f15909a3b
jackhoo_98 <jackhoo_98@foxmail.com>
2023-02-10 15:12:04 +0800

    update: 更新Modal组件，支持拖拽功能

--- components/components.ts
+++ components/components.ts
@@ -1,12 +1,13 @@
-export { default as Pagination} from './Pagination'
-export { default as TableCard} from './TableCard'
-export { default as Scrollbar} from './Scrollbar'
+export { default as Pagination } from './Pagination';
+export { default as TableCard } from './TableCard';
+export { default as Scrollbar } from './Scrollbar';
 export { default as AIcon } from './AIcon';
-export { default as Empty } from './Empty'
+export { default as Empty } from './Empty';
 export { default as Tree } from './Tree';
 export { default as AutoComplete } from './AutoComplete';
 export { default as Message } from './Message';
-export { default as Input} from './Input'
-export { default as Textarea} from './Textarea'
-export { default as MonacoEditor} from './MonacoEditor'
-export { default as Popconfirm} from './Popconfirm'
+export { default as Input } from './Input';
+export { default as Textarea } from './Textarea';
+export { default as MonacoEditor } from './MonacoEditor';
+export { default as Popconfirm } from './Popconfirm';
+export { default as Modal } from './Modal';
