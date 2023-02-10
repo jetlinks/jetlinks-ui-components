c3d8ca8aff66dd70e6092a2b0e2898f3aa0826bf
linyi <92132544+holt230@users.noreply.github.com>
2023-02-10 11:33:43 +0800

    refactor: 迁移Divider组件

--- components/Divider/divider.vue
+++ components/Divider/divider.vue
@@ -1,21 +1,13 @@
 <template>
-    <Divider v-bind="props" class="jl-divider" />
+  <Divider v-bind="props">
+    <slot></slot>
+  </Divider>
 </template>
 
 <script lang="ts" setup>
 import { Divider } from 'ant-design-vue';
-import { dividerProps } from 'ant-design-vue/es/divider';
-import { defineProps } from 'vue';
+import { dividerProps } from 'ant-design-vue/lib/divider';
 
 // 定义属性
-const props = defineProps({
-    ...dividerProps(),
-});
+const props = defineProps(dividerProps());
 </script>
-
-<style scoped lang="less">
-.jl-divider {
-    height: 1px;
-    background-color: #e9e9e9;
-}
-</style>
