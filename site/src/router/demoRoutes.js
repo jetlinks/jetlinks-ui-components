5cce00d91390dc103f9085751a383f7b448f1dff
胡彪 <1679575151@qq.com>
2023-02-10 17:29:20 +0800

    feat:  新增Spin组件

--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -7,12 +7,14 @@ export default [
     component: () => import('../../../components/AIcon/demo/index.vue'),
   },
   {
-    path: 'Empty',
-    meta: {"category":"Components","type":"数据展示","title":"Empty","subtitle":"空状态","cover":"https://gw.alipayobjects.com/zos/alicdn/MNbKfLBVb/Empty.svg"},
-    component: () => import('../../../components/Empty/demo/index.vue'),
     path: 'AutoComplete',
     meta: {"category":"Components","subtitle":"自动完成","type":"数据录入","cols":2,"title":"AutoComplete","cover":"https://gw.alipayobjects.com/zos/alicdn/qtJm4yt45/AutoComplete.svg"},
     component: () => import('../../../components/AutoComplete/demo/index.vue'),
+  },
+  {
+    path: 'Ellipsis',
+    meta: {"category":"Components","title":"Ellipsis","subtitle":"文本省略","cover":"https://mdn.alipayobjects.com/huamei_7uahnr/afts/img/A*7sMiTbzvaDoAAAAAAAAAAAAADrJ8AQ/original","cols":2,"type":"布局","order":3},
+    component: () => import('../../../components/Ellipsis/demo/index.vue'),
   },
   {
     path: 'Empty',
@@ -30,19 +32,29 @@ export default [
     component: () => import('../../../components/Message/demo/index.vue'),
   },
   {
-    path: 'MonacoEditor',
-    meta: {"category":"Components","type":"数据录入","title":"MonacoEditor","subtitle":"代码编辑器","cols":1,"cover":"https://gw.alipayobjects.com/zos/alicdn/xS9YEJhfe/Input.svg"},
-    component: () => import('../../../components/MonacoEditor/demo/index.vue'),
-  },
-  {
     path: 'Pagination',
     meta: {"category":"Components","subtitle":"分页","cols":1,"type":"导航","title":"Pagination","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
     component: () => import('../../../components/Pagination/demo/index.vue'),
   },
   {
-    path: 'Select',
-    meta: {"category":"Components","subtitle":"选择器","type":"数据录入","cols":2,"title":"Select","cover":"https://gw.alipayobjects.com/zos/alicdn/qtJm4yt45/AutoComplete.svg"},
-    component: () => import('../../../components/Select/demo/index.vue'),
+    path: 'MonacoEditor',
+    meta: {"category":"Components","type":"数据录入","title":"MonacoEditor","subtitle":"代码编辑器","cols":1,"cover":"https://gw.alipayobjects.com/zos/alicdn/xS9YEJhfe/Input.svg"},
+    component: () => import('../../../components/MonacoEditor/demo/index.vue'),
+  },
+  {
+    path: 'TableCard',
+    meta: {"category":"Components","subtitle":"表格卡片","cols":2,"type":"导航","title":"TableCard","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
+    component: () => import('../../../components/TableCard/demo/index.vue'),
+  },
+  {
+    path: 'Tabs',
+    meta: {"category":"Components","subtitle":"标签页","type":"数据展示","title":"Tabs","cover":"https://gw.alipayobjects.com/zos/antfincdn/lkI2hNEDr2V/Tabs.svg"},
+    component: () => import('../../../components/Tabs/demo/index.vue'),
+  },
+  {
+    path: 'Tree',
+    meta: {"category":"Components","type":"数据展示","title":"Tree","subtitle":"树形控件","cover":"https://gw.alipayobjects.com/zos/alicdn/Xh-oWqg9k/Tree.svg"},
+    component: () => import('../../../components/Tree/demo/index.vue'),
   },
   {
     path: 'Popconfirm',
@@ -59,9 +71,4 @@ export default [
     meta: {"category":"Components","subtitle":"表格卡片","cols":2,"type":"导航","title":"TableCard","cover":"https://gw.alipayobjects.com/zos/alicdn/1vqv2bj68/Pagination.svg"},
     component: () => import('../../../components/TableCard/demo/index.vue'),
   },
-  {
-    path: 'Tree',
-    meta: {"category":"Components","type":"数据展示","title":"Tree","subtitle":"树形控件","cover":"https://gw.alipayobjects.com/zos/alicdn/Xh-oWqg9k/Tree.svg"},
-    component: () => import('../../../components/Tree/demo/index.vue'),
-  }
 ];
