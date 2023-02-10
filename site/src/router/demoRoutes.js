2bb1f1b60c16544784dec19e4e60140ec9954290
haodd0216 <62583365+haodd0216@users.noreply.github.com>
2023-02-10 15:42:00 +0800

    update: Empty默认图片更换
    * update: Empty默认图片更换

--- site/src/router/demoRoutes.js
+++ site/src/router/demoRoutes.js
@@ -7,6 +7,9 @@ export default [
     component: () => import('../../../components/AIcon/demo/index.vue'),
   },
   {
+    path: 'Empty',
+    meta: {"category":"Components","type":"数据展示","title":"Empty","subtitle":"空状态","cover":"https://gw.alipayobjects.com/zos/alicdn/MNbKfLBVb/Empty.svg"},
+    component: () => import('../../../components/Empty/demo/index.vue'),
     path: 'AutoComplete',
     meta: {"category":"Components","subtitle":"自动完成","type":"数据录入","cols":2,"title":"AutoComplete","cover":"https://gw.alipayobjects.com/zos/alicdn/qtJm4yt45/AutoComplete.svg"},
     component: () => import('../../../components/AutoComplete/demo/index.vue'),
