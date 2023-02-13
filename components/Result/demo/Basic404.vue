699ecd710243f13b27ba516d839267b74adc9a23
jackhoo_98 <jackhoo_98@foxmail.com>
2023-02-13 14:13:46 +0800

    feat:  完成Result组件

---
+++ components/Result/demo/Basic404.vue
@@ -0,0 +1,20 @@
+<docs>
+---
+order: 1
+title: 404
+---
+  
+此页面未找到。  
+
+  </docs>
+<template>
+    <j-result
+        status="404"
+        sub-title="Sorry, the page you visited does not exist."
+    >
+        <template #extra>
+            <a-button type="primary" ghost>Back Home</a-button>
+        </template>
+    </j-result>
+</template>
+<style scoped></style>
---
+++ components/Result/demo/basic.vue
@@ -0,0 +1,21 @@
+<docs>
+---
+order: 1
+title: success
+---
+
+成功的结果。
+
+</docs>
+<template>
+    <j-result
+        status="success"
+        title="Successfully Purchased Cloud Server ECS!"
+        sub-title="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+    >
+        <template #extra>
+            <a-button key="console" type="primary">Go Console</a-button>
+            <a-button key="buy">Buy Again</a-button>
+        </template>
+    </j-result>
+</template>
