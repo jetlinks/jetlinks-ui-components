d4d510911e840e4fa5e12aa4c64280bc4220977d
jackhoo_98 <jackhoo_98@foxmail.com>
2023-02-13 14:39:53 +0800

    update: 更新Spin组件，恢复默认样式

--- components/Spin/spin.vue
+++ components/Spin/spin.vue
@@ -3,9 +3,6 @@
         <template v-for="item in Object.keys(slots)" :key="item">
             <slot :name="item"></slot>
         </template>
-        <template v-if="!Object.keys(slots).includes('indicator')" #indicator>
-            <img class="spinning" src="./logo.png" />
-        </template>
     </Spin>
 </template>
 
@@ -21,51 +18,4 @@ const props = defineProps({
 });
 </script>
 
-<style lang="less" scoped>
-.spinning {
-    -webkit-transition-property: -webkit-transform;
-    -webkit-transition-duration: 1s;
-    -moz-transition-property: -moz-transform;
-    -moz-transition-duration: 1s;
-    -webkit-animation: rotate 1.2s linear infinite;
-    -moz-animation: rotate 1.2s linear infinite;
-    -o-animation: rotate 1.2s linear infinite;
-    animation: rotate 1.2s linear infinite;
-}
-
-@-webkit-keyframes rotate {
-    from {
-        -webkit-transform: rotate(0deg);
-    }
-    to {
-        -webkit-transform: rotate(360deg);
-    }
-}
-
-@-moz-keyframes rotate {
-    from {
-        -moz-transform: rotate(0deg);
-    }
-    to {
-        -moz-transform: rotate(359deg);
-    }
-}
-
-@-o-keyframes rotate {
-    from {
-        -o-transform: rotate(0deg);
-    }
-    to {
-        -o-transform: rotate(359deg);
-    }
-}
-
-@keyframes rotate {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(359deg);
-    }
-}
-</style>
+<style lang="less" scoped></style>
