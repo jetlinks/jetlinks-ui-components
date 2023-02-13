d86fd1bf377bafcd8ceaf06645e1c4fcecde21ce
胡彪 <1679575151@qq.com>
2023-02-13 15:10:03 +0800

    delete: 删除Spin组件
    * feat:  完成Spin组件
    
    * update: 更新Spin组件，恢复默认样式
    
    * update: 删除Spin组件相关代码
    
    ---------
    
    Co-authored-by: jackhoo_98 <jackhoo_98@foxmail.com>

--- components/components.ts
+++ components/components.ts
@@ -15,7 +15,6 @@ export { default as Ellipsis } from './Ellipsis';
 export { default as Popconfirm } from './Popconfirm';
 export { default as Divider } from './Divider';
 export { default as Modal } from './Modal';
-export { default as Spin } from './Spin';
 export { default as Descriptions, JDescriptionsItem } from './Descriptions'
 export { default as Button} from './Button'
 export { default as Result } from './Result';
