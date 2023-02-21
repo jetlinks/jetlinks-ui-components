400ffdbee4a6be2510905997970d86370962d28f
jackhoo_98 <jackhoo_98@foxmail.com>
2023-02-21 11:43:59 +0800

    update: 修改Message导出方式

--- components/Message/index.ts
+++ components/Message/index.ts
@@ -1,7 +1,3 @@
-import { withInstall } from '../util/type';
-import Message from './demo/index.vue';
-export { JMessage } from '../Message/messageType'
+import { JMessage } from '../Message/messageType';
 
-export const JLMessage = withInstall(Message, 'JMessage');
-
-export default JLMessage;
+export default JMessage;
