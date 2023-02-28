22fbdda2e7cd480ac0e9aed30f6eb9446751b9ef
xieyonghong <18010623010@163.com>
2023-02-28 11:02:58 +0800

    update: 优化BasicLayout

--- components/MonacoEditor/monaco.vue
+++ components/MonacoEditor/monaco.vue
@@ -7,30 +7,6 @@
 import * as monaco from 'monaco-editor';
 import { ref, onMounted } from 'vue';
 
-// import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-// import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-// import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-// import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-// import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-//
-// self.MonacoEnvironment = {
-//     getWorker(_: string, label: string) {
-//         if (label === 'json') {
-//             return new jsonWorker();
-//         }
-//         if (label === 'css') {
-//             return new cssWorker();
-//         }
-//         if (label === 'html') {
-//             return new htmlWorker();
-//         }
-//         if (['typescript', 'javascript'].includes(label)) {
-//             return new tsWorker();
-//         }
-//         return new editorWorker();
-//     },
-// };
-
 const props = defineProps({
     value: { type: String, default: '' },
     theme: { type: String, default: 'vs-dark' },
