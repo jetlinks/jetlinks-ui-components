d56fb20b70c8bec0ef3c62cbbd34bdb53c2fb0ed
haodd0216 <62583365+haodd0216@users.noreply.github.com>
2023-03-02 20:19:46 +0800

    style: 组件依赖样式引入
    * update: tablecard组件更新参数设置
    
    * update: tablecard组件更新，状态内置及自定义
    
    * feat: 描述列表组件
    
    * 主题色修改，tablecard参数类型修改
    
    * update: 更新table-card组件
    
    * update: 更新descriptions组件，style引入
    
    * update: 更新tablecard样式引入
    
    * style: 更新TableCard组件样式
    
    * feat: 新增组件：Affix、Alert、Anchor、Avatar
    
    * feat: 新增组件：BackTop、Breadcrumb
    
    * feat: 新增组件：Calendar、Carousel、Cascader、Checkbox、Col、Collapse、Comment
    
    * feat: 新增组件：DatePicker、Drawer、Dropdown、
    
    * feat: 新增组件：Form、Grid
    
    * feat: 新增组件：Image、InputNumber
    
    * feat: 新增组件：Mentions、Menu
    
    * feat: 新增组件：Notification、PageHeader、Popover、Progress
    
    * feat: 新增组件：Radio、Rate、Row、Skeleton、Silder
    
    * feat: 新增组件：Space、Spin、Statistic、Steps、Switch
    
    * feat: 新增组件：Table、Tag、Timeline、Tooltip、Transfer、TreeSelect、
    
    * update: components.ts文件修改、input添加导出组件、
    
    * update: 引入组件style，代码规范
    
    * feat: 新增antdv原生组件card
    
    * update: datepicker时间选择器导出MonthPicker,WeekPicker,RangePicker,QuarterPicker
    
    * updte: descriptions样式文件引入
    
    * updte: components引入card组件和badge组件，select、divider、button、input样式文件引入
    
    * update：组件引入依赖样式文件
    
    * update：monaco-editor更新

--- components/MonacoEditor/monaco.vue
+++ components/MonacoEditor/monaco.vue
@@ -3,26 +3,50 @@
     <div ref="dom" class="editor"></div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import * as monaco from 'monaco-editor';
-import { ref, onMounted } from 'vue';
+
+// import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+// import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+// import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+// import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+// import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+//
+// self.MonacoEnvironment = {
+//     getWorker(_, label) {
+//         if (label === 'json') {
+//             return new jsonWorker();
+//         }
+//         if (label === 'css') {
+//             return new cssWorker();
+//         }
+//         if (label === 'html') {
+//             return new htmlWorker();
+//         }
+//         if (['typescript', 'javascript'].includes(label)) {
+//             return new tsWorker();
+//         }
+//         return new editorWorker();
+//     },
+// };
 
 const props = defineProps({
-    value: { type: String, default: '' },
+    modelValue: [String, Number],
     theme: { type: String, default: 'vs-dark' },
     language: { type: String, default: 'json' },
 });
 
-const emit = defineEmits(['update:value']);
+const emit = defineEmits(['update:modelValue']);
 
 const dom = ref();
 
-let instance: any;
+let instance;
 
 onMounted(() => {
-    const model = monaco.editor.createModel(props.value, props.language);
+    const _model = monaco.editor.createModel(props.modelValue, props.language);
+
     instance = monaco.editor.create(dom.value, {
-        model: model,
+        model: _model,
         tabSize: 2,
         automaticLayout: true,
         scrollBeyondLastLine: false,
@@ -32,7 +56,65 @@ onMounted(() => {
 
     instance.onDidChangeModelContent(() => {
         const value = instance.getValue();
-        emit('update:value', value);
+        emit('update:modelValue', value);
     });
 });
+
+/**
+ * 代码格式化
+ */
+const editorFormat = () => {
+    if (!instance) return;
+    instance.getAction('editor.action.formatDocument')?.run();
+};
+
+watchEffect(() => {
+    setTimeout(() => {
+        editorFormat();
+    }, 300);
+});
+
+/**
+ * 光标位置插入内容
+ * @param {String} val
+ */
+const insert = (val) => {
+    if (!instance) return;
+    const position = instance.getPosition();
+    instance.executeEdits(instance.getValue(), [
+        {
+            range: new monaco.Range(
+                position?.lineNumber,
+                position?.column,
+                position?.lineNumber,
+                position?.column,
+            ),
+            text: val,
+        },
+    ]);
+};
+
+watch(
+    () => props.modelValue,
+    (val) => {
+        if (!instance) return;
+        // setValue之前获取光标位置
+        const position = instance.getPosition();
+        // setValue之后光标位置改变
+        instance.setValue(val);
+        // 设置光标位置为setValue之前的位置
+        instance.setPosition(position);
+    },
+);
+
+defineExpose({
+    editorFormat,
+    insert,
+});
 </script>
+
+<style lang="less" scoped>
+.editor {
+    height: 100%;
+}
+</style>
