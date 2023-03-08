ca5a293e1165aa36579df806c3655903ed82f588
haodd0216 <62583365+haodd0216@users.noreply.github.com>
2023-03-08 09:00:50 +0800

    feat: 样式名修改，inputNumber组件添加demo
    * update: tablecard组件更新参数设置
    
    * update: tablecard组件更新，状态内置及自定义
    
    * feat: 描述列表组件
    
    * 主题色修改，tablecard参数类型修改
    
    * update: 更新table-card组件
    
    * update: 更新descriptions组件，style引入
    
    * update: 更新tablecard样式引入
    
    * style: 更新TableCard组件样式
    
    * feat: 新增组件：Affix、Alert、Anchor、Avatar
    
    * feat: 新增组件：BackTop、Breadcrumb
    
    * feat: 新增组件：Calendar、Carousel、Cascader、Checkbox、Col、Collapse、Comment
    
    * feat: 新增组件：DatePicker、Drawer、Dropdown、
    
    * feat: 新增组件：Form、Grid
    
    * feat: 新增组件：Image、InputNumber
    
    * feat: 新增组件：Mentions、Menu
    
    * feat: 新增组件：Notification、PageHeader、Popover、Progress
    
    * feat: 新增组件：Radio、Rate、Row、Skeleton、Silder
    
    * feat: 新增组件：Space、Spin、Statistic、Steps、Switch
    
    * feat: 新增组件：Table、Tag、Timeline、Tooltip、Transfer、TreeSelect、
    
    * update: components.ts文件修改、input添加导出组件、
    
    * update: 引入组件style，代码规范
    
    * feat: 新增antdv原生组件card
    
    * update: datepicker时间选择器导出MonthPicker,WeekPicker,RangePicker,QuarterPicker
    
    * updte: descriptions样式文件引入
    
    * updte: components引入card组件和badge组件，select、divider、button、input样式文件引入
    
    * update：组件引入依赖样式文件
    
    * update：monaco-editor更新
    
    * update：组件依赖样式引入文件修改
    
    * update：修改样式引入
    
    * update：修改样式引入
    
    * fix: steps组件引入缺失
    
    * update：修改样式引入
    
    * update：组件注册，demo补齐
    
    * fix: button在disabled情况下 Tootip没有提示
    
    * update: result组件样式文件引入
    
    * update: demo补全
    
    * fix: 在TSX里面使用Descriptions.Item方式失效
    
    * update: collapse组件导出
    
    * update: search组件根据不同情况返回不同的搜索结果
    
    * style: 样式名修改

---
+++ components/InputNumber/demo/digit.vue
@@ -0,0 +1,35 @@
+<docs>
+---
+order: 3
+title: 高精度小数
+---
+
+## zh-CN
+
+通过 `stringMode` 开启高精度小数支持，`change` 事件将返回 string 类型。
+对于旧版浏览器，你需要 BigInt polyfill。
+
+</docs>
+
+<template>
+    <j-input-number
+        v-model:value="value"
+        style="width: 200px"
+        :min="0"
+        :max="10"
+        :step="0.00000000000001"
+        string-mode
+    />
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    setup() {
+        const value = ref<string>('1');
+
+        return {
+            value,
+        };
+    },
+});
+</script>
---
+++ components/InputNumber/demo/out-of-range.vue
@@ -0,0 +1,30 @@
+<docs>
+---
+order: 6
+title: 超出边界
+---
+
+## zh-CN
+
+当通过受控将 `value` 超出边界时，提供警告样式。
+
+</docs>
+
+<template>
+    <j-space>
+        <j-input-number v-model:value="value" :min="1" :max="10" />
+        <j-button type="primary" @click="value = 99">Reset</j-button>
+    </j-space>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    setup() {
+        const value = ref<number>(99);
+
+        return {
+            value,
+        };
+    },
+});
+</script>
