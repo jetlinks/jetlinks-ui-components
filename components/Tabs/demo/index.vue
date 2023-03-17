1122614ee3eb07b3037151972be37d9aec986e48
haodd0216 <62583365+haodd0216@users.noreply.github.com>
2023-03-17 09:53:41 +0800

    style: 修改tabs样式

--- components/Tabs/demo/index.vue
+++ components/Tabs/demo/index.vue
@@ -2,17 +2,20 @@
   <demo-sort :cols="1">
     <Basic />
     <Extra />
+    <Card />
   </demo-sort>
 </template>
 <script lang="ts">
 import Basic from './basic.vue'
 import Extra from './extra.vue'
+import Card from './card.vue'
 import CN from '../index.md'
 export default {
   CN,
   components: {
     Basic,
-    Extra
+    Extra,
+    Card
   }
 }
 </script>
