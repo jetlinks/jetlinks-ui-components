de7058a0d99a0fe194844cf14cfd91c231711644
xieyonghong <18010623010@163.com>
2023-03-24 17:33:49 +0800

    update: 修改proTable中节流为防抖

--- components/ProTable/demo/index.vue
+++ components/ProTable/demo/index.vue
@@ -1,21 +1,24 @@
 <template>
-  <demo-sort :cols="1">
-    <Basic />
-    <Promise />
-    <Selection />
-  </demo-sort>
+    <demo-sort :cols="1">
+        <Basic />
+        <Promise />
+        <Selection />
+        <Search />
+    </demo-sort>
 </template>
 <script lang="ts">
-import Basic from './basic.vue'
-import Promise from './promise.vue'
-import Selection from './selection.vue'
-import CN from '../index.md'
+import Basic from './basic.vue';
+import Promise from './promise.vue';
+import Selection from './selection.vue';
+import Search from './search.vue';
+import CN from '../index.md';
 export default {
-  CN,
-  components: {
-    Basic,
-    Promise,
-    Selection,
-  }
-}
+    CN,
+    components: {
+        Basic,
+        Promise,
+        Selection,
+        Search,
+    },
+};
 </script>
