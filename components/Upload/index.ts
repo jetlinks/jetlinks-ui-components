b5e1b46e73984fcb29be7c29633844bf1f1ae699
XieYongHong <18010623010@163.com>
2023-06-09 17:13:29 +0800

    fix: 修复upload无法使用dragger问题

--- components/Upload/index.ts
+++ components/Upload/index.ts
@@ -1,12 +1,15 @@
 import type { App } from 'vue';
 import JUploadImage from './upload.vue';
 import Upload from 'ant-design-vue/lib/upload';
+import Dragger from 'ant-design-vue/lib/dropdown';
 
 Upload.name = 'JUpload';
+Dragger.name = 'JUploadDragger';
 
 Upload.install = function (app: App) {
     app.component('JUpload', Upload);
     app.component('JUploadImage', JUploadImage);
+    app.component(Dragger.name, Dragger);
     return app;
 };
 
