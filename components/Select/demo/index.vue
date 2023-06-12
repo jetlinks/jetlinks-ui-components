7c4f76fdcff86a638005acd9c0e36185baab21c4
XieYongHong <18010623010@163.com>
2023-06-12 18:07:36 +0800

    feat: 新增j-select-boolean组件

--- components/Select/demo/index.vue
+++ components/Select/demo/index.vue
@@ -1,18 +1,21 @@
 <template>
     <demo-sort>
         <Basic />
+        <Boolean />
         <Optgroup />
     </demo-sort>
 </template>
 <script lang="ts">
 import Basic from './basic.vue';
 import Optgroup from './optgroup.vue';
+import Boolean from './boolean.vue';
 import CN from '../index.md';
 export default {
     CN,
     components: {
         Basic,
-        Optgroup
+        Optgroup,
+      Boolean
     },
 };
 </script>
