4e38df86b55faaee1c91c79f8ee77ab61db84f6b
hrc1457 <2507849421@qq.com>
2023-08-10 17:40:40 +0800

    Avatar

--- components/Avatar/index.ts
+++ components/Avatar/index.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue';
 import Avatar from 'ant-design-vue/lib/avatar/Avatar';
 import AvatarGroup from 'ant-design-vue/lib/avatar/Group';
 
-export type { AvatarProps } from 'ant-design-vue/lib/avatar/index';
+export type { AvatarProps, AvatarSize, AvatarGroupProps } from 'ant-design-vue/lib/avatar/index';
 
 Avatar.name = 'JAvatar';
 AvatarGroup.name = 'JAvatarGroup';
