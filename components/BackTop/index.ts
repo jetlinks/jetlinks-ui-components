16736b1ff2bb8c52a8e1f9e8d2e748b31385637c
hrc1457 <2507849421@qq.com>
2023-08-10 16:35:01 +0800

    BackTop

--- components/BackTop/index.ts
+++ components/BackTop/index.ts
@@ -1,5 +1,8 @@
 import { withInstall } from '../util/type';
 import BackTop from 'ant-design-vue/lib/back-top/index';
+import { backTopProps } from 'ant-design-vue/lib/back-top/index'
+
+export {backTopProps}
 
 export type { BackTopProps } from 'ant-design-vue/lib/back-top/index';
 
