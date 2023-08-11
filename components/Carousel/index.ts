3e187f5aa731809e4244969fb70b324a74078731
hrc1457 <2507849421@qq.com>
2023-08-11 10:43:59 +0800

    Carousel

--- components/Carousel/index.ts
+++ components/Carousel/index.ts
@@ -1,7 +1,14 @@
 import { withInstall } from '../util/type';
 import Carousel from 'ant-design-vue/lib/carousel/index';
 
-export type { CarouselProps } from 'ant-design-vue/lib/carousel/index';
+export type { CarouselProps, 
+              SwipeDirection, 
+              LazyLoadTypes, 
+              CarouselEffect,  
+              DotPosition,
+              CarouselRef } from 'ant-design-vue/lib/carousel/index';
+
+export { carouselProps } from 'ant-design-vue/lib/carousel/index';
 
 export const JCarousel = withInstall(Carousel, 'JCarousel');
 
