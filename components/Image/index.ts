777320d6046d86ba533d56d7505c4b1b3cf19ac2
hrc1457 <2507849421@qq.com>
2023-08-15 14:07:23 +0800

    Image

--- components/Image/index.ts
+++ components/Image/index.ts
@@ -3,9 +3,10 @@ import Image from 'ant-design-vue/lib/image/index';
 import ImagePreviewGroup from 'ant-design-vue/lib/image/PreviewGroup';
 
 export type { ImageProps } from 'ant-design-vue/lib/image/index';
+export {  imageProps } from 'ant-design-vue/lib/image/index';
 
-Image.name = 'JBadge';
-ImagePreviewGroup.name = 'JBadgeRibbon';
+Image.name = 'JImage';
+ImagePreviewGroup.name = 'JImageRibbon';
 
 Image.install = function (app: App) {
     app.component(Image.name, Image);
