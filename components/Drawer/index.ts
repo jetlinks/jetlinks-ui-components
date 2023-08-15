9f4a5af5efcfec5361b6acf07490d818a34ae8ba
hrc1457 <2507849421@qq.com>
2023-08-15 10:58:15 +0800

    Drawer

--- components/Drawer/index.ts
+++ components/Drawer/index.ts
@@ -1,7 +1,8 @@
 import { withInstall } from '../util/type';
 import Drawer from 'ant-design-vue/lib/drawer/index';
 
-export type { DrawerProps } from 'ant-design-vue/lib/drawer/index';
+export { drawerProps } from 'ant-design-vue/lib/drawer/index';
+export type { DrawerProps, placementType, sizeType, PushState } from 'ant-design-vue/lib/drawer/index';
 
 export const JDrawer = withInstall(Drawer, 'JDrawer');
 
