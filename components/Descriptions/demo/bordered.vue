1b8d1be6ecf3b157aaa77e462f0d6c9496f4f931
hrc1457 <58102991+hrc1457@users.noreply.github.com>
2023-08-16 11:36:28 +0800

    fix：修改Descriptions导出，添加demo
    * Alert
    
    * Breadcrumb
    
    * Carousel
    
    * Cascader
    
    * Checkbox
    
    * Collapse
    
    * Comment
    
    * ConfigProvider
    
    * DatePicker
    
    * Descriptions

--- components/Descriptions/demo/bordered.vue
+++ components/Descriptions/demo/bordered.vue
@@ -10,16 +10,17 @@ title: 带边框的
 
 <template>
   <j-descriptions title="User Info" bordered  size="middle">
-    <j-descriptions-item label="Product"></j-descriptions-item>
+    <j-descriptions-item label="Product">Cloud Database</j-descriptions-item>
     <j-descriptions-item label="Billing Mode">Prepaid</j-descriptions-item>
-    <j-descriptions-item label="Automatic Renewal"></j-descriptions-item>
-    <j-descriptions-item label="Order time"></j-descriptions-item>
+    <j-descriptions-item label="Automatic Renewal">YES</j-descriptions-item>
+    <j-descriptions-item label="Order time">2018-04-24 18:00:00</j-descriptions-item>
     <j-descriptions-item label="Usage Time" :span="2">2019-04-24 18:00:00</j-descriptions-item>
     <j-descriptions-item label="Status" :span="3">
+      <j-badge status="processing" text="Running" />
     </j-descriptions-item>
-    <j-descriptions-item label="Negotiated Amount"></j-descriptions-item>
+    <j-descriptions-item label="Negotiated Amount">$80.00</j-descriptions-item>
     <j-descriptions-item label="Discount">$20.00</j-descriptions-item>
-    <j-descriptions-item label="Official Receipts"></j-descriptions-item>
+    <j-descriptions-item label="Official Receipts">$60.00</j-descriptions-item>
     <j-descriptions-item label="Config Info">
       Data disk type: MongoDB
       <br />
