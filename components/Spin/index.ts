bcb732259f1744b2995b283e19614526a749f13e
hrc1457 <2507849421@qq.com>
2023-08-18 14:15:07 +0800

    fix：修改Spin导出

--- components/Spin/index.ts
+++ components/Spin/index.ts
@@ -2,6 +2,7 @@ import { withInstall } from '../util/type';
 import Spin from 'ant-design-vue/lib/spin/Spin';
 
 export type { SpinProps } from 'ant-design-vue/lib/spin/index';
+export { spinProps } from 'ant-design-vue/lib/spin/index';
 
 export const JSpin = withInstall(Spin, 'JSpin');
 
