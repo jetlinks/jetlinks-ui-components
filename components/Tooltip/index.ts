feff731f507b66b3f9f08a0ae7a305cb73980cfb
hrc1457 <2507849421@qq.com>
2023-08-21 10:32:14 +0800

    fix：修改Tooltip导出

--- components/Tooltip/index.ts
+++ components/Tooltip/index.ts
@@ -1,7 +1,16 @@
 import { withInstall } from '../util/type';
 import Tooltip from 'ant-design-vue/lib/tooltip/Tooltip';
 
-export type { TooltipProps } from 'ant-design-vue/lib/tooltip/index';
+export type {
+    TooltipProps,
+    AdjustOverflow,
+    PlacementsConfig,
+    TooltipAlignConfig,
+    TooltipPlacement,
+  } from 'ant-design-vue/lib/tooltip/index';
+
+  export { tooltipProps } from 'ant-design-vue/lib/tooltip/index';
+
 
 export const JTooltip = withInstall(Tooltip, 'JTooltip');
 
