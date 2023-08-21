ab38fdd20dda084b5cb45a17771617c11029330c
hrc1457 <58102991+hrc1457@users.noreply.github.com>
2023-08-21 19:59:55 +0800

    fix：修改Transfer导出和md文件组件名称

--- components/Transfer/index.ts
+++ components/Transfer/index.ts
@@ -1,7 +1,22 @@
 import { withInstall } from '../util/type';
 import Transfer from 'ant-design-vue/lib/transfer/index';
 
-export type { TransferProps } from 'ant-design-vue/lib/transfer/index';
+export type { 
+    TransferProps,
+    TransferListProps,
+    TransferOperationProps,
+    TransferSearchProps,
+    TransferDirection,
+    RenderResult,
+    TransferItem,
+    KeyWise,
+    KeyWiseTransferItem,
+    ListStyle,
+    SelectAllLabel,
+    TransferLocale
+} from 'ant-design-vue/lib/transfer/index';
+
+export { transferProps } from 'ant-design-vue/lib/transfer/index';
 
 export const JTransfer = withInstall(Transfer, 'JTransfer');
 
