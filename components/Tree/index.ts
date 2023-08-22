7bfa7fa398d266a43374e085a2e0c7a71464b1ed
hrc1457 <2507849421@qq.com>
2023-08-22 10:17:10 +0800

    fix：修改Tree中TreeNode和DirectoryTree名称

--- components/Tree/index.ts
+++ components/Tree/index.ts
@@ -1,5 +1,5 @@
 import { withInstall } from '../util/type';
-import Tree from 'ant-design-vue/lib/tree';
+import Tree, { TreeNode, DirectoryTree } from 'ant-design-vue/lib/tree';
 export { treeFilter } from '../util/filterNodeMethod';
 
 export type {
@@ -18,9 +18,12 @@ export type {
     DirectoryTreeProps
 } from 'ant-design-vue/lib/tree';
 
-export { DirectoryTree, TreeNode } from 'ant-design-vue/lib/tree';
-
 Tree.name = 'JTree';
+TreeNode.name = 'JTreeNode'
+DirectoryTree.name = 'JDirectoryTree'
+
+export { TreeNode, DirectoryTree }
+
 export const JLTree = withInstall(Tree, 'JTree');
 
 export default JLTree;
