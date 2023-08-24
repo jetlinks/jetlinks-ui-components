462d90ca69af0a185e296191f16a6f638b2b5e12
100006368 <1234.com>
2023-08-24 18:02:13 +0800

    feat: 新增monaco-editor组件支持vue语言

--- components/MonacoEditor/monaco.vue
+++ components/MonacoEditor/monaco.vue
@@ -5,7 +5,7 @@
 
 <script setup lang="ts">
 import * as monaco from 'monaco-editor';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, PropType } from 'vue';
 
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
@@ -13,12 +13,18 @@ import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
+// vue support
+import * as onigasm from 'onigasm';
+import onigasmWasm from 'onigasm/lib/onigasm.wasm?url';
+import { loadGrammars, loadTheme } from 'monaco-volar';
+import vueWorker from 'monaco-volar/vue.worker?worker';
+
 self.MonacoEnvironment = {
     getWorker(_: string, label: string) {
         if (label === 'json') {
             return new jsonWorker();
         }
-        if (label === 'css') {
+        if (label === 'css' || label === 'scss' || label === 'less') {
             return new cssWorker();
         }
         if (label === 'html') {
@@ -27,36 +33,59 @@ self.MonacoEnvironment = {
         if (['typescript', 'javascript'].includes(label)) {
             return new tsWorker();
         }
+        if (label === 'vue') {
+            return new vueWorker();
+        }
         return new editorWorker();
     },
 };
 
 const props = defineProps({
     value: { type: String, default: '' },
-    theme: { type: String, default: 'vs-dark' },
+    /*主题色: vs(默认高亮), vs-dark(黑色), hc-black(高亮黑色) */
+    theme: {
+        type: String as PropType<'vs' | 'vs-dark' | 'hc-black'>,
+        default: 'vs-dark',
+    },
     language: { type: String, default: 'json' },
 });
 
-const emit = defineEmits(['update:value']);
-
+const emit = defineEmits(['update:value', 'change']);
 const dom = ref();
-
 let instance: any;
 
-onMounted(() => {
+const getTheme = async () => {
+    if (props.language === 'vue') {
+        const theme = await loadTheme(monaco.editor);
+        if (props.theme === 'vs') {
+            return theme.light;
+        } else if (props.theme === 'vs-dark') {
+            return theme.dark;
+        }
+        return theme.dark;
+    }
+    return props.theme;
+};
+
+onMounted(async () => {
     const model = monaco.editor.createModel(props.value, props.language);
     instance = monaco.editor.create(dom.value, {
         model: model,
         tabSize: 2,
         automaticLayout: true,
         scrollBeyondLastLine: false,
-        theme: props.theme, // 主题色: vs(默认高亮), vs-dark(黑色), hc-black(高亮黑色)
+        theme: await getTheme(),
         formatOnPaste: true,
     });
 
     instance.onDidChangeModelContent(() => {
         const value = instance.getValue();
         emit('update:value', value);
+        emit('change', value);
     });
+    if (props.language === 'vue') {
+        onigasm.loadWASM(onigasmWasm);
+        loadGrammars(monaco, instance);
+    }
 });
 </script>
